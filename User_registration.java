a7f64a62dd3adcdebe77878d4fe965e1b718f0c5
navedkhan0809 <navedkhan0809@gmail.com>
2021-05-11 22:48:10 +0530

    UC1 added

--- User_registration.java
+++ User_registration.java
@@ -1,10 +1,30 @@
 package com.userRegistration;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class User_registration {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Welcome to user registration problem");
+		boolean res;
+		Scanner s = new Scanner(System.in);
 		
+		System.out.println("Enter first name: ");
+		String firstname = s.nextLine();
+			
+		String nameRegex = "^[A-Z]{1}[a-z]{2,}$";
+			
+		Pattern p = Pattern.compile(nameRegex);
+		Matcher m = p.matcher(firstname);
+		res  = m.find();
+		  
+		if (!res) {
+		  	System.out.println("Invalid Input");
+		   
+		}else {
+		   	System.out.println("Correct input");
+		}
 	}
 }
