d93916c5be00fab22f4058b54b86d5fbc207707b
navedkhan0809 <navedkhan0809@gmail.com>
2021-05-11 22:31:01 +0530

    Welcome message added on master

---
+++ User_registration.java
@@ -0,0 +1,10 @@
+package com.userRegistration;
+
+public class User_registration {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to user registration problem");
+		
+	}
+}
